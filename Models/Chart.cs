17d6da01a1e89185be8a066964cf65e907ae8e2f
Hassan88N <40647959+Hassan88N@users.noreply.github.com>
2021-12-13 23:57:06 +0100

    Add files via upload

---
+++ Models/Chart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace AzureAPP.Models
+{
+    public class Chart
+    {
+        public int measurementId { get; set; }
+        public string sensorName { get; set; }
+        public float measurementValue { get; set; }
+
+        public string measurementDateTime { get; set; }
+
+        public List<Chart> GetChartData(string connectionString)
+        {
+            List<Chart> chartDataList = new List<Chart>();
+            SqlConnection con = new SqlConnection(connectionString);
+            string selectSQL = "Select MeasurementId, SensorName, ReferenceValue, MeasurementValue, Unit, MeasurementDateTime from MeasurementDatabase";//
+            con.Open();
+            SqlCommand cmd = new SqlCommand(selectSQL, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr != null)
+            {
+                while (dr.Read())
+                {
+                    Chart chartData = new Chart();
+                    chartData.measurementId = Convert.ToInt32(dr["MeasurementId"]);
+                    chartData.measurementDateTime = dr["MeasurementDateTime"].ToString();
+                    chartData.measurementValue = Convert.ToSingle(dr["MeasurementValue"]);
+                    //chartData.measurementValue = Convert.ToSingle(dr["ReferenceValue"]); //
+                    chartDataList.Add(chartData);
+                }
+            }
+            return chartDataList;
+        }
+    }
+}
---
+++ Models/Measurement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AzureAPP.Models
+{
+    public class Measurement
+    {
+        public int measurementId { get; set; }
+        public string sensorName { get; set; }
+        public float measurementValue { get; set; }
+        public float referenceValue { get; set; }
+
+        public string unit { get; set; }
+        public string measurementDateTime { get; set; }
+        
+
+        public List<Measurement> GetMeasurementParameters(string connectionString)
+        {
+            List<Measurement> measurementParameterList = new List<Measurement>();
+
+            
+            SqlConnection con = new SqlConnection(connectionString);
+            string sqlQuery = "Select MeasurementId, SensorName, ReferenceValue, MeasurementValue, Unit, MeasurementDateTime from MeasurementDatabase";//
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+
+            if (dr != null)
+            {
+                while (dr.Read())
+                {
+                    Measurement measurementParameter = new Measurement();
+                    measurementParameter.measurementId = Convert.ToInt32(dr["MeasurementId"]);
+                    measurementParameter.sensorName = dr["SensorName"].ToString(); //
+                    measurementParameter.referenceValue = Convert.ToSingle(dr["ReferenceValue"]);
+                    measurementParameter.measurementValue = Convert.ToSingle(dr["MeasurementValue"]);
+                    measurementParameter.unit = dr["Unit"].ToString(); //
+                    measurementParameter.measurementDateTime = dr["MeasurementDateTime"].ToString();
+                    measurementParameterList.Add(measurementParameter);
+                }
+
+            }
+            return measurementParameterList;
+
+
+        }
+    }
+}
