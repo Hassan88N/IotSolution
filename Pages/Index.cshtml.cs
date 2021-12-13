e6fc96db3cc6933077e003e45e63f4fea8d5329d
Hassan88N <40647959+Hassan88N@users.noreply.github.com>
2021-12-13 23:56:37 +0100

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Data.SqlClient;
+
+namespace AzureAPP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string sensorName;
+        public string measurementValue;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ///string connectionString = "Data Source=NAWHASW10\\MSSQLSERVER1; Initial Catalog=DATA;Integrated Security=True;TrustServerCertificate=True";
+            //SqlConnection con = new SqlConnection(connectionString);
+            //con.Open();
+
+            //string sqlQuery = "Select SensorName, MeasurementValue from MeasurementD where MeasurementId=80 ";
+            //SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            //SqlDataReader dr = cmd.ExecuteReader();
+            //if (dr.Read())
+            {
+            //    sensorName = dr["SensorName"].ToString();
+            //    measurementValue = dr["MeasurementValue"].ToString();
+            }
+            //con.Close();
+        }
+    }
+}
---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AzureAPP.Models;
+using Microsoft.Extensions.Configuration;
+namespace AzureAPP.Pages
+{
+
+    public class PrivacyModel : PageModel
+    {
+        public List<Chart> chartDataList = new List<Chart>();//
+
+        readonly IConfiguration _configuration;
+        public List<Measurement> measurementParameterList = new List<Measurement>();
+        public string connectionString;
+
+        public PrivacyModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            Measurement measurement = new Measurement();
+            connectionString = _configuration.GetConnectionString("ConnectionString");
+            measurementParameterList = measurement.GetMeasurementParameters(connectionString);
+
+            chartDataList = ChartData(); //
+
+        }
+        private List<Chart> ChartData() //
+        {
+            connectionString = _configuration.GetConnectionString("ConnectionString");
+            List<Chart> chartDataList = new List<Chart>();
+            Chart chartData = new Chart();
+            chartDataList = chartData.GetChartData(connectionString);
+            return chartDataList;
+        }
+    }
+}
